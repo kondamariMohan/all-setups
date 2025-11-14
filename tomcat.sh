41acd0475497105e90ab176c8fe1376695497a44
kondamariMohan <mohanreddy2516@gmail.com>
2025-11-14 18:23:53 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -9,3 +9,4 @@ sed -i '56d' apache-tomcat-9.0.112/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-9.0.112/webapps/manager/META-INF/context.xml
 sed -i '22d'  apache-tomcat-9.0.112/webapps/manager/META-INF/context.xml
 sh apache-tomcat-9.0.112/bin/startup.sh
+
